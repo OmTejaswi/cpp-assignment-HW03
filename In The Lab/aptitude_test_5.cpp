49df24d44228450a98df0adb8ae3d1cb8b50d386
OmTejaswi <75362653+OmTejaswi@users.noreply.github.com>
2021-07-24 14:15:29 +0530

    updated

--- In The Lab/aptitude_test_5.cpp
+++ In The Lab/aptitude_test_5.cpp
@@ -3,7 +3,20 @@
 #include <iostream>
 
 int main() {
-    int ana, log, gen;
+    int ana, log, gk;
+    std::cout << "Enter the marks obtained in Analytical paper: ";
+    std::cin >> ana;
 
-    
+    std::cout << "Enter the marks obtained Logical paper: ";
+    std::cin >> log;
+
+    std::cout << "Enter the marks obtained in General Knowledge paper: ";
+    std::cin >> gk;
+
+    if(ana > 50 || log > 50 || gk > 50) {
+        std::cout << "Please enter a number less than 50";
+    } else if(ana <= 50 || log <= 50 || gk <= 50) {
+        std::cout << "The weighted marks are: " << 0.4*ana+0.3*log+0.3*gk;
+    }
+
 }
